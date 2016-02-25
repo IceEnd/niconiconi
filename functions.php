ac1596bdc75adc126cbb0a34dc6af57bbb179667
Cononico <min@coolecho.net>
2016-02-25 13:20:16 +0800

    2016/2/25 关闭Wordpress4.x 响应式图片功能。

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ if ( function_exists('register_sidebar') )
 
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 800, 800, true );
-
+add_filter( 'max_srcset_image_width', create_function( '', 'return 1;' ) );
 
 
 function autoset_featured() {
